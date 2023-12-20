2391fee8643b6afb448bcec6ca429f920639dba3
Yuliya Kotova <yulya9449@yandex.ru>
2023-12-21 02:16:16 +0300

    refactor: solution binary-search-tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -2,10 +2,10 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const { Node } = require('../extensions/list-tree.js');
 
-// /**
-// * Implement simple binary search tree according to task description
-// * using Node from extensions
-// */
+/**
+* Implement simple binary search tree according to task description
+* using Node from extensions
+*/
 
 //!---------------------------
 
@@ -13,9 +13,9 @@ const { Node } = require('../extensions/list-tree.js');
 // node.data - это цифра
 
 class BinarySearchTree {
-    constructor() {
-        this.rootNode = null;
-    }
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
     return this.rootNode;
@@ -24,166 +24,136 @@ class BinarySearchTree {
   add(data) {
     this.rootNode = addNode(this.rootNode, data);
 
-    function addNode(node, data) {
-        if (!node) {
-            node = new Node(data);
-            return node;
-            // return new Node(data);
-        }
+    function addNode(node, val) {
+      if (!node) {
+        node = new Node(val);
+      }
 
-        if (node.data === data) {
-            return node;
-        }
+      if (node.data > val) {
+        node.left = addNode(node.left, val);
+      }
 
-        if (node.data > data) {
-            node.left = addNode(node.left, data)
-        } else if (node.data < data) {
-            node.right = addNode(node.right, data)
-        }
+      if (node.data < val) {
+        node.right = addNode(node.right, val);
+      }
 
-        return node;//узел возвращается в вызвавшую функцию, а т.к. она сама себя вызывала,
-        // то он возвращается в предыдущий уровень рекурсии
+      return node; // узел возвращается в вызвавшую функцию,
+      // а т.к. она сама себя вызывала,
+      // то он возвращается в предыдущий уровень рекурсии
     }
   }
 
   has(data) {
-
-    function toSearchNode(node, data) {
-        if (!node) {
-            return false;
-        }
-
-        if (node.data === data) {
-            return true;
-        }
-
-        if (node.data > data) {
-            return toSearchNode(node.left, data)
-        } else if (node.data < data) {
-            return toSearchNode(node.right, data)
-        }
-    }
-
-    return toSearchNode(this.rootNode, data)
-
+    return !!this.find(data);
   }
 
   find(data) {
+    return toFind(this.rootNode, data);
 
-    if (!this.has(data)) {
-        return null
-    }
+    function toFind(node, val) {
+      if (!node) {
+        return null;
+      }
 
-    // if (this.has(data)) {
-        return toFind(this.rootNode, data);
-    // }
+      if (node.data === val) {
+        return node;
+      }
 
-    function toFind(node, data) {
-        if (node.data === data) {
-            return node;
-        }
+      if (node.data > val) {
+        return toFind(node.left, val);
+      }
 
-        if (node.data > data) {
-            return toFind(node.left, data);
-        }
-
-        if (node.data < data) {
-            return toFind(node.right, data)
-        }
-
+      if (node.data < val) {
+        return toFind(node.right, val);
+      }
     }
   }
 
   remove(data) {
-    if (!this.has(data)) {
-        return null
-    }
+    this.rootNode = removeNode(this.rootNode, data); // в каком
+    // поддереве нужно удалить узел и с каким значением
 
-    this.rootNode = removeNode(this.rootNode, data);// в каком поддереве нужно удалить узел и с каким значением
+    function removeNode(node, val) {
+      if (node.data > val) {
+        node.left = removeNode(node.left, val);
+        return node;
+      }
 
-    function removeNode(node, data) {
-        if (node.data > data) {
-            node.left = removeNode(node.left, data);
-            return node;
-        } else if (node.data < data) {
-            node.right = removeNode(node.right, data);
-            return node;
-        } else if (node.data === data) {
+      if (node.data < val) {
+        node.right = removeNode(node.right, val);
+        return node;
+      }
 
-            if(!node.left && !node.right) {
-                return null;//если лист, то вместо него кладём null
-            }
-            // если есть только правое поддерево
-            if(!node.left && node.right) {
-                node = node.right;
-                return node;
-            }
-            // если есть только левое поддерево
-            if(!node.right && node.left) {
-                node = node.left;
-                return node;
-            }
-            // если есть и левое и правое поддеревья
-            if(node.right && node.left) {
-                // ищем максимальный элемент слева
-                let maxLeft = node.left;
-
-                while(maxLeft.right) {
-                    maxLeft = maxLeft.right;// нашли максимальный элемент слева
-                }
-
-                node.data = maxLeft.data;//заменяем значение удаляемого узла на максимальное слева
-
-                node.left = removeNode(node.left, maxLeft.data);//удаляем этот максимальный элемент из поддерева
-
-                return node;
-            }
+      if (node.data === val) {
+        if (!node.left && !node.right) {
+          // если лист, то вместо него кладём null
+          return null;
         }
-    }
 
+        // если есть только правое поддерево
+        if (!node.left && node.right) {
+          node = node.right;
+          return node;
+        }
+
+        // если есть только левое поддерево
+        if (!node.right && node.left) {
+          node = node.left;
+          return node;
+        }
+
+        // если есть и левое и правое поддеревья
+        if (node.right && node.left) {
+          let maxLeft = node.left; // ищем максимальный элемент слева
+
+          while (maxLeft.right) {
+            maxLeft = maxLeft.right; // нашли максимальный элемент слева
+          }
+
+          node.data = maxLeft.data; // заменяем значение удаляемого узла на максимальное слева
+
+          node.left = removeNode(node.left, maxLeft.data); // удаляем
+          // этот максимальный элемент из поддерева
+
+          return node;
+        }
+      }
+    }
   }
 
   min() {
     if (!this.rootNode) {
-        return null
+      return null;
     }
 
     let node = this.rootNode;
 
     while (node.left) {
-        node = node.left;
+      node = node.left;
     }
 
     return node.data;
-
-
   }
 
   max() {
     if (!this.rootNode) {
-        return null
+      return null;
     }
 
     let node = this.rootNode;
 
     while (node.right) {
-        node = node.right
+      node = node.right;
     }
 
     return node.data;
-
   }
 }
 
-
-
 //!----------------------------
-
-// //!
-// console.log(tree.root());
 
 module.exports = {
   BinarySearchTree
 };
 
-// npm run test ./test/binary-search-tree*
+// npm run test ./test/binary*
